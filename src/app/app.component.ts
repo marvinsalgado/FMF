3a8d45f983704cb294ea91a4abd349c716c86c02
jhmata <jhmata@fmf.mx>
2021-03-24 10:22:28 -0600

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'jr-dev';
+  title = 'jr-test';
+  
+  ngOnInit(): void
+  {   
+
+  }
 }
